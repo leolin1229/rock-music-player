f802a20a45aac09d8d9fab93c60dce9c21081a15
leolin1229 <787659058@qq.com>
2014-02-23 04:55:51 +0800

    'update'

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,58 @@
 $(document).ready(function() {
+	"use strict";
+	var mp3 = {
+		// 计算Tag大小
+		'getTagSize': function(str) {
+			if(typeof str === 'string' && str.length === 4) {
+            	var total = (str.charCodeAt(0)&0x7f)*0x200000 
+                	      + (str.charCodeAt(1)&0x7f)*0x400
+                    	  + (str.charCodeAt(2)&0x7f)*0x80
+                    	  + (str.charCodeAt(3)&0x7f);
+                return total;
+        	} else {
+        		throw Error('参数错误!');
+        	}
+		},
+		// 计算帧大小
+		'getFrameSize': function(str) {
+			if(typeof str === 'string' && str.length === 4) {
+            	var total = str.charCodeAt(0)*0x100000000
+            	+ str.charCodeAt(1)*0x10000
+            	+ str.charCodeAt(2)*0x100
+            	+ str.charCodeAt(3);
+            	return total;
+        
+        	} else {
+            	throw Error('参数错误！');
+        	}
+		},
+		// 帧列表
+    	'Frames': {
+        	'TPE1' :'作者',
+        	'TIT2' :'标题',
+        	'TYER' :'年代',
+        	'APIC' :'专辑图片',
+        	'TALB' :'专辑'
+    	},
+    	// 获取专辑图片
+    	'getAlbumPic': function(result) {
+    		// 专辑信息位置
+        	var index = result.indexOf('APIC');
+        
+        	if(index < 0) {
+        	    alert('找不到专辑图片');
+        	    return false;
+        	}
+        	// 图片信息大小
+        	var picsize = getFrameSize(result.substr(index+4, 4)),
+        	// 图片信息块1
+            pic1 = result.substr(index+10 , picsize),
+        	// 图片信息块2
+            pic2 = pic1.slice(pic1.indexOf('\xff\xd8'));
+            
+        	return pic2;
+    	}
+	};
 	// 全局变量
 	var mGalleryIndex = 0;
 	var mGalleryReader = null;
@@ -7,6 +61,8 @@ $(document).ready(function() {
 	var mGalleryData = [];
 	var curOptGrp = null;
 	var audFormats = ['wav', 'mp3'];
+	var localMusicList = [];
+	var onlineMusicList = [];
 
 	// 打印错误信息
 	function errorHandler(custom) {
@@ -95,22 +151,32 @@ $(document).ready(function() {
    			childDiv2.append('<span class="list-songname">' + itemEntry.name + '</span>');
    			parentDiv.append(childDiv2);
    		}
-   		// div.appendChild(document.createTextNode(itemEntry.name));
-   		// curOptGrp.appendChild(div);
+
    		$("#GalleryList").append(parentDiv);
 	}
 
-	function scanSongs(fs) {
-		var mData = chrome.mediaGalleries.getMediaFileSystemMetadata(fs);
+	function addScanTips(path) {
+		$(".scanTips").append("<span class='songname-tips'>" + path + "</span>");
+		$(".scanTipsWrapper").show();
+	}
+
+	function deleteScanTips() {
+		$(".scanTipsWrapper").hide();
+	}
+
+	function scanSongs(DOMFileSystem) {
+		var mData = chrome.mediaGalleries.getMediaFileSystemMetadata(DOMFileSystem);// Object
 
 		console.log('Reading directory: ' + mData.name);
-		curOptGrp = addSong(mData.name, mData.galleryId);
-		mGalleryData[mGalleryIndex] = new SongData(mData.galleryId);
-		mGalleryReader = fs.root.createReader();
+
+		// curOptGrp = addSong(mData.name, mData.galleryId);
+		mGalleryData[mGalleryIndex] = new SongData(mData.galleryId);// new SongData object
+		mGalleryReader = DOMFileSystem.root.createReader();
 		mGalleryReader.readEntries(scanSong, errorHandler('readEntries'));
 	}
 
-	function scanSong(entries) {
+	// 递归
+	function scanSong(entries) {// FileEntry
 		if(entries.length == 0) {
 			if(mGalleryDirectories.length > 0) {
 				var dir_entry = mGalleryDirectories.shift();
@@ -128,16 +194,48 @@ $(document).ready(function() {
 			return ;
 		}
 		for(var i = 0; i < entries.length; i++) {
-			console.log(entries[i].name);
 
 			if(entries[i].isFile) {
+				// 大小，结束时间
+				entries[i].file(function success(details) {
+					// console.log(details);
+					var blob = details.slice(0, 3, 'MIME');
+					var reader = new FileReader();
+					reader.readAsText(blob);
+					reader.onprogress = function(e) {};//
+					reader.onload = function(e) {
+						console.log(e.target.result);
+					};
+
+					
+				}, errorHandler("fadsf"));
+
 				addItem(entries[i]);
+
+				var mData = chrome.mediaGalleries.getMediaFileSystemMetadata(mGalleryArray[mGalleryIndex]);
+
+				// 添加正在解析某首音乐提示
+				var fullPath = '';
+				if(checkOs() == "MS") {
+					fullPath = mData.name + entries[i].fullPath.replace(/\//g, "\\");
+				}else {
+					fullPath = mData.name + entries[i].fullPath;
+				}
+
+				addScanTips(fullPath);
+
+				// 媒体库信息处理
 				mGalleryData[mGalleryIndex].numFiles++;
-				(function(mData) {
+				(function(gData) {
 					entries[i].getMetadata(function(metadata) {
-						mData.sizeBytes += metadata.size;
+						gData.sizeBytes += metadata.size;
 					});
 				}(mGalleryData[mGalleryIndex]));
+
+
+
+				// 删除提示
+				deleteScanTips();
 			}else if(entries[i].isDirectory) {
 				mGalleryDirectories.push(entries[i]);
 			}else {
@@ -148,6 +246,20 @@ $(document).ready(function() {
 		mGalleryReader.readEntries(scanSong, errorHandler('readMoreEntries'));
 	}
 
+	function checkOs() {
+		var windows = (navigator.userAgent.indexOf("Windows",0) != -1)?1:0;
+		var mac = (navigator.userAgent.indexOf("mac",0) != -1)?1:0;
+		var linux = (navigator.userAgent.indexOf("Linux",0) != -1)?1:0;
+		var unix = (navigator.userAgent.indexOf("X11",0) != -1)?1:0;
+ 		
+ 		var os_type = '';
+		if (windows) os_type = "MS";
+		else if (mac) os_type = "Apple";
+		else if (linux) os_type = "Lunix";
+		else if (unix) os_type = "Unix";
+ 
+		return os_type;
+	}
 	///////////////////////////// app初始化 /////////////////////////////
 	var init = function() {
 		$("#local").addClass('menu-active');
@@ -162,6 +274,7 @@ $(document).ready(function() {
 		$("#leftCol2-singerList").hide();
 		$(".shade").hide();
 		$(".layer").hide();
+		$(".scanTipsWrapper").hide();
 	}();
 
 	// 本地音乐和在线音乐标签切换
@@ -207,7 +320,7 @@ $(document).ready(function() {
 			 	return false;
 			});
 		}else {
-			var str = "<span>没有任何音频文件哦～</span>";
+			var str = "<span>没有任何音频文件夹哦～</span>";
 			$(".layer-body-content").html(str);
 		}
 	}
@@ -220,12 +333,18 @@ $(document).ready(function() {
 
 	$("#scanBtn").click(function(event) {
 		if(mGalleryArray.length > 0) {
-			scanSongs(mGalleryArray[0]);
+			scanSongs(mGalleryArray[0]);// DOMFileSystem of Array
 		}
+		$(".shade").hide();
+		$(".layer").hide();
 	});
 
 	$("#cancelBtn").click(function(event) {
 		$(".shade").hide();
 		$(".layer").hide();
 	});
+
+	// chrome.mediaGalleries.onScanProgress.addListener(function callback(details) {
+	// 	console.log(details);
+	// });
 });
